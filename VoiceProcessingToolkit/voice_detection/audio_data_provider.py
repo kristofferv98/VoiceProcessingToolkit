43aa4574fc7833742b4e289a89cfad965175a725
Kristofferv98 <kristoffer.vatnehol@gmail.com>
2023-12-19 15:47:07 +0100

    aider: Implemented PyAudioDataProvider class for audio data frames retrieval.
    # Aider chat conversation:
    
    ASSISTANT: Understood. Let's proceed with the correct file path and the implementation of the `AudioDataProvider` in `VoiceProcessingToolkit/voice_detection/audio_data_provider.py`.
    
    Here is the *SEARCH/REPLACE block* for the `VoiceProcessingToolkit/voice_detection/audio_data_provider.py` file:
    
    VoiceProcessingToolkit/voice_detection/audio_data_provider.py
    ```python
    <<<<<<< SEARCH
    =======
    # === IMPLEMENTATION OF AUDIO DATA PROVIDER INTERFACE ===
    
    import pyaudio
    import numpy as np
    
    class PyAudioDataProvider:
        """
        PyAudioDataProvider provides audio data frames using PyAudio.
        """
    
        def __init__(self, format=pyaudio.paInt16, channels=1, rate=16000, frames_per_buffer=1024):
            """
            Initialize the PyAudioDataProvider with audio stream parameters.
            Args:
                format: The sample format.
                channels: The number of audio channels.
                rate: The sample rate.
                frames_per_buffer: The number of frames per buffer.
            """
            self.py_audio = pyaudio.PyAudio()
            self.stream = self.py_audio.open(format=format, channels=channels, rate=rate, input=True, frames_per_buffer=frames_per_buffer)
    
        def get_audio_frame(self):
            """
            Returns a single frame of audio data from the stream.
            """
            frame = self.stream.read(frames_per_buffer)
            return np.frombuffer(frame, dtype=np.int16)
    
        def cleanup(self):
            """
            Clean up the audio stream and PyAudio instance.
            """
            self.stream.stop_stream()
            self.stream.close()
            self.py_audio.terminate()
    >>>>>>> REPLACE
    ```
    
    This block will create the `PyAudioDataProvider` class in the `audio_data_provider.py` file within the `VoiceProcessingToolkit/voice_detection` directory. The class includes the `__init__`, `get_audio_frame`, and `cleanup` methods as specified.

--- VoiceProcessingToolkit/voice_detection/audio_data_provider.py
+++ VoiceProcessingToolkit/voice_detection/audio_data_provider.py
@@ -85,3 +85,39 @@ class PyAudioDataProvider(AudioDataProvider):
         self.stream.stop_stream()
         self.stream.close()
         self.py_audio.terminate()
+# === IMPLEMENTATION OF AUDIO DATA PROVIDER INTERFACE ===
+
+import pyaudio
+import numpy as np
+
+class PyAudioDataProvider:
+    """
+    PyAudioDataProvider provides audio data frames using PyAudio.
+    """
+
+    def __init__(self, format=pyaudio.paInt16, channels=1, rate=16000, frames_per_buffer=1024):
+        """
+        Initialize the PyAudioDataProvider with audio stream parameters.
+        Args:
+            format: The sample format.
+            channels: The number of audio channels.
+            rate: The sample rate.
+            frames_per_buffer: The number of frames per buffer.
+        """
+        self.py_audio = pyaudio.PyAudio()
+        self.stream = self.py_audio.open(format=format, channels=channels, rate=rate, input=True, frames_per_buffer=frames_per_buffer)
+
+    def get_audio_frame(self):
+        """
+        Returns a single frame of audio data from the stream.
+        """
+        frame = self.stream.read(frames_per_buffer)
+        return np.frombuffer(frame, dtype=np.int16)
+
+    def cleanup(self):
+        """
+        Clean up the audio stream and PyAudio instance.
+        """
+        self.stream.stop_stream()
+        self.stream.close()
+        self.py_audio.terminate()
