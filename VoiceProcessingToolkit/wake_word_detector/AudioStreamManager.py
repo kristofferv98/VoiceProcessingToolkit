9728a5e1c8c2e836b0c8b195f33bf511ed667897
Kristofferv98 <kristoffer.vatnehol@gmail.com>
2023-12-19 14:31:22 +0100

    aider: Rename `format` parameter to `audio_format` in `AudioStreamManager` class.
    # Aider chat conversation:
    
    ASSISTANT: To address the issue of shadowing the built-in name `format` in the `AudioStreamManager` class, we need to modify the `AudioStreamManager.py` file. We will rename the `format` parameter to avoid conflicts with the built-in function `format`.
    
    Here is the *SEARCH/REPLACE block* for the necessary changes:
    
    VoiceProcessingToolkit/wake_word_detector/AudioStreamManager.py
    <source>python
    <<<<<<< SEARCH
        def __init__(self, rate: int, channels: int, format: int, frames_per_buffer: int):
            self.py_audio = pyaudio.PyAudio()
            self.stream = self._initialize_stream(rate, channels, format, frames_per_buffer)
    
        def _initialize_stream(self, rate: int, channels: int, format: int, frames_per_buffer: int):
            try:
                return self.py_audio.open(rate=rate, channels=channels, format=format,
    =======
        def __init__(self, rate: int, channels: int, audio_format: int, frames_per_buffer: int):
            self.py_audio = pyaudio.PyAudio()
            self.stream = self._initialize_stream(rate, channels, audio_format, frames_per_buffer)
    
        def _initialize_stream(self, rate: int, channels: int, audio_format: int, frames_per_buffer: int):
            try:
                return self.py_audio.open(rate=rate, channels=channels, format=audio_format,
    >>>>>>> REPLACE
                                          input=True, frames_per_buffer=frames_per_buffer)
            except pyaudio.PyAudio as e:
                logger.exception("Failed to initialize audio stream: %s", e)
                raise
            except Exception as e:
                logger.exception("An unexpected error occurred while initializing the audio stream.", exc_info=e)
                raise
    </source>
    
    This change renames the `format` parameter to `audio_format` in both the `__init__` and `_initialize_stream` methods to avoid shadowing the built-in `format` function.

--- VoiceProcessingToolkit/wake_word_detector/AudioStreamManager.py
+++ VoiceProcessingToolkit/wake_word_detector/AudioStreamManager.py
@@ -5,13 +5,13 @@ logger = logging.getLogger(__name__)
 
 
 class AudioStreamManager:
-    def __init__(self, rate: int, channels: int, format: int, frames_per_buffer: int):
+    def __init__(self, rate: int, channels: int, audio_format: int, frames_per_buffer: int):
         self.py_audio = pyaudio.PyAudio()
-        self.stream = self._initialize_stream(rate, channels, format, frames_per_buffer)
+        self.stream = self._initialize_stream(rate, channels, audio_format, frames_per_buffer)
 
-    def _initialize_stream(self, rate: int, channels: int, format: int, frames_per_buffer: int):
+    def _initialize_stream(self, rate: int, channels: int, audio_format: int, frames_per_buffer: int):
         try:
-            return self.py_audio.open(rate=rate, channels=channels, format=format,
+            return self.py_audio.open(rate=rate, channels=channels, format=audio_format,
                                       input=True, frames_per_buffer=frames_per_buffer)
         except pyaudio.PyAudio as e:
             logger.exception("Failed to initialize audio stream: %s", e)
