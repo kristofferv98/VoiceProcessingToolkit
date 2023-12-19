e50afc19d76083c68dc7997dad41d0207c5d610f
Kristofferv98 <kristoffer.vatnehol@gmail.com>
2023-12-19 15:16:56 +0100

    Deleted AudioDataProvider interface for providing audio data to the VAD.

--- audio_data_provider.py
+++ audio_data_provider.py
@@ -1,13 +1,24 @@
-# === INTERFACES / ABSTRACT CLASSES SECTION ===
-class AudioDataProvider:
-    """
-    Interface for providing audio data to the VAD.
+# === IMPLEMENTATION OF AUDIO DATA PROVIDER INTERFACE ===
 
-    Implement this interface in a separate module or file where the actual audio data handling is defined.
+import pyaudio
+import numpy as np
+
+class PyAudioDataProvider(AudioDataProvider):
     """
+    PyAudioDataProvider provides audio data frames using PyAudio.
+    """
+
+    def __init__(self, stream):
+        """
+        Initialize the PyAudioDataProvider with an audio stream.
+        Args:
+            stream: An instance of PyAudio stream to read audio data from.
+        """
+        self.stream = stream
 
     def get_audio_frame(self):
         """
-        Should be implemented to return a single frame of audio data.
+        Returns a single frame of audio data from the stream.
         """
-        raise NotImplementedError
+        frame = self.stream.read(self.stream.get_read_available(), exception_on_overflow=False)
+        return np.frombuffer(frame, dtype=np.int16)
